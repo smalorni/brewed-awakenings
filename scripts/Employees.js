2e517bdb9f8f49baf0ce30eb4c1fd55540c0bf50
Samantha Malorni <samantha.ann.malorni@gmail.com>
2022-05-13 15:38:44 -0400

    updated changes to employees and products

--- scripts/Employees.js
+++ scripts/Employees.js
@@ -1,8 +1,39 @@
 import { getEmployees } from "./database.js"
+//needed to import getOrders to have access to array of orders
+import { getOrders } from "./database.js"
 
 const employees = getEmployees()
+const orderLists = getOrders()
+
+const soldProducts = (employee) => {
+    let productOrders = [];
+
+        for (const order of orderLists) {
+            if(order.employeeId === employee.id)
+                productOrders.push(order)
+        }
+        return productOrders
+}
+
 
 export const Employees = () => {
+    document.addEventListener (
+        "click",
+        (clickEvent) => {
+            const itemClicked = clickEvent.target
+                if(itemClicked.id.startsWith("employee")) {
+                const [ ,employeeId ] = itemClicked.id.split("--")
+                    for(const employee of employees) {
+                    if(employee.id === parseFloat(employeeId)) {
+                        let completedOrders = soldProducts(employee)
+                        //how do we get the length on an array - use .length method
+                        window.alert(`${employee.name} sold ${completedOrders.length} products`)
+                    }
+                }
+            }
+        }
+    )
+
     let html = "<ul>"
 
     for (const employee of employees) {
--- scripts/Products.js
+++ scripts/Products.js
@@ -2,7 +2,25 @@ import { getProducts } from "./database.js"
 
 const products = getProducts()
 
+
 export const Products = () => {
+
+    document.addEventListener (
+        "click",
+        (clickEvent) => {
+            const itemClicked = clickEvent.target
+            if(itemClicked.id.startsWith("product")) {
+                const [ ,productId ] =itemClicked.id.split("--")
+    
+                for(const product of products) {
+                    if(product.id === parseFloat(productId)) {
+                        window.alert(`${product.name} costs $${product.price}`)
+                    }
+                }
+            }
+        }
+    )
+
     let html = "<ul>"
 
     for (const product of products) {
